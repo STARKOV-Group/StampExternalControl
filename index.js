0e77a76c71eb94fa03ed5d7851bc0f971fea544b
Vershinin <vershinin@starkovgrp.ru>
2024-03-20 15:35:53 +0500

    Добавлен режим отладки start:dev:standalone

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // Точка входа для отладки контролов в режиме standalone (отдельное приложение, без веб клиента Sungero).
+import api from './host-api-stub';
+import loadApp from './src/loaders/stamp-control-loader';
+import { ILoaderArgs } from '@directum/sungero-remote-component-types';
 
-loadApp(document.getElementById('app'), context, api);
+let args = {
+    /** Контейнер. */
+    container: document.getElementById('app'),
+    /** Контекст инициализации. */
+    initialContext: null,
+    /** API сторонних компонентов. */
+    api: api,
+    /** Параметры стороннего контрола.*/
+    controlInfo: null
+}
+loadApp(args);
