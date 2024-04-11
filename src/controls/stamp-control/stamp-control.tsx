e79bd58673c8beb9771e985b7e8807ed9b412830
Vershinin <vershinin@starkovgrp.ru>
2024-04-11 15:00:51 +0500

    Доработка функционала для работы с несколькими штампами

--- src/controls/stamp-control/stamp-control.tsx
+++ src/controls/stamp-control/stamp-control.tsx
@@ -1,73 +1,73 @@
 import React, { MouseEventHandler, useCallback, useEffect, useRef, useState } from 'react';
-import { IRemoteComponentCardApi, ControlUpdateHandler } from '@directum/sungero-remote-component-types';
+import { IRemoteComponentCardApi, ControlUpdateHandler, IChildEntityCollection } from '@directum/sungero-remote-component-types';
 import { ICustomEntity, IPagesRow, IStampInfoRow } from './types';
 import './stamp-control.css'
-import { number } from 'yargs';
 
 interface IProps {
     api: IRemoteComponentCardApi;
 }
 
 const StampControl: React.FC<IProps> = ({ api }) => {
-    //#region Entity
+    //#region Props
     const [entity, setEntity] = useState<ICustomEntity>(() => api.getEntity<ICustomEntity>());
     const [pageInfo, setPageInfo] = useState<IPagesRow | undefined>(entity.Pagesstarkov.find(() => true));
+    const [stampInfo, setStampInfo] = useState<IChildEntityCollection<ICustomEntity, IStampInfoRow>>(entity.StampInfostarkov);
     const [currentPageNumber, setCurrentPageNumber] = useState<number>(1);
     const [currentStampId, setCurrentStampId] = useState<number>();
+    const [coordsText, setCoordsText] = useState('');
     const isLocked = entity.LockInfo && entity.LockInfo.IsLocked && (!entity.LockInfo.IsLockedByMe || !entity.LockInfo.IsLockedHere);
     const isEnabled = entity.State.IsEnabled && !isLocked;
+    const isClicked = useRef(false);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const boxRefs = stampInfo
+        .filter(row => row.PageNumber == currentPageNumber)
+        .map(row => {
+            return ({
+                boxRef: useRef<HTMLDivElement>(null),
+                stampId: row.Id,
+                coords: useRef<{
+                    startX: number,
+                    startY: number,
+                    lastX: number,
+                    lastY: number
+                }>({
+                    startX: 0,
+                    startY: 0,
+                    lastX: 0,
+                    lastY: 0
+                })
+            });
+        });
+    //#endregion
 
+    //#region Entity
     const handleControlUpdate: ControlUpdateHandler = useCallback(() => {
         setEntity(api.getEntity<ICustomEntity>());
-        setPageInfo(entity.Pagesstarkov.find(() => true));
+        setStampInfo;
+        setPageInfo;
         setCurrentPageNumber(1);
     }, [api, setEntity]);
     api.onControlUpdate = handleControlUpdate;
 
-    const handleCoordinateChange = (coordX: number, coordY: number) => {
-        entity.StampInfostarkov.forEach((row) => {
-            row?.changeProperty('CoordX', coordX);
-            row?.changeProperty('CoordY', coordY);
-        });
+    const handleCoordinateChange = (stampId: number | undefined, coordX: number, coordY: number) => {
+        var currentStamp = stampInfo.find(x => x.Id == stampId);
+        currentStamp?.changeProperty('CoordX', coordX);
+        currentStamp?.changeProperty('CoordY', coordY);
     };
 
     useEffect(() => {
         showStamps();
-    }, [entity.StampInfostarkov]);
+    }, [setStampInfo]);
 
     useEffect(() => {
         setCurrentPageNumber(pageInfo?.Number ?? 1);
         updateBackgroundImage(pageInfo);
         showStamps();
-        updateCoordsFromEntity();
         setBtnState();
-    }, [pageInfo]);
+    }, [setPageInfo]);
     //#endregion
 
     //#region DragControl
-    const [coordsText, setCoordsText] = useState('');
-    const containerRef = useRef<HTMLDivElement>(null);
-    const boxRefs = entity.StampInfostarkov
-        .filter(row => row.PageNumber == currentPageNumber)
-        .map(row => {
-            return ({
-                boxRef: useRef<HTMLDivElement>(null),
-                stampId: row.Id
-            });
-        });
-    const isClicked = useRef(false);
-    const coords = useRef<{
-        startX: number,
-        startY: number,
-        lastX: number,
-        lastY: number
-    }>({
-        startX: 0,
-        startY: 0,
-        lastX: 0,
-        lastY: 0
-    })
-
     function pxToDot(num: number) {
         return num / 96 * 72;
     }
@@ -77,7 +77,9 @@ const StampControl: React.FC<IProps> = ({ api }) => {
     }
 
     useEffect(() => {
-        var boxRef = boxRefs.find(x => x.stampId == currentStampId)?.boxRef;
+        var currentStamp = boxRefs.find(x => x.stampId == currentStampId);
+        var coords = currentStamp?.coords.current;
+        var boxRef = currentStamp?.boxRef;
         if (!boxRef?.current || !containerRef.current || !isEnabled)
             return;
 
@@ -85,27 +87,33 @@ const StampControl: React.FC<IProps> = ({ api }) => {
         const container = containerRef.current;
         const onMouseDown = (e: MouseEvent) => {
             isClicked.current = true;
-            coords.current.startX = e.clientX;
-            coords.current.startY = e.clientY;
+            if (!coords)
+                return;
+
+            coords.startX = e.clientX;
+            coords.startY = e.clientY;
         }
         const onMouseUp = (e: MouseEvent) => {
             isClicked.current = false;
-            if (coords.current.lastX === box.offsetLeft && coords.current.lastY === box.offsetTop)
+            if (!coords)
                 return;
 
-            coords.current.lastX = box.offsetLeft;
-            coords.current.lastY = box.offsetTop;
+            if (coords.lastX === box.offsetLeft && coords.lastY === box.offsetTop)
+                return;
+
+            coords.lastX = box.offsetLeft;
+            coords.lastY = box.offsetTop;
             let convertedX = pxToDot(box.offsetLeft);
             let convertedY = pxToDot(box.offsetTop);
             setCoordsText(`X: ${convertedX.toFixed(1)}, Y: ${convertedY.toFixed(1)}`);
-            handleCoordinateChange(convertedX, convertedY);
+            handleCoordinateChange(currentStamp?.stampId, convertedX, convertedY);
         }
         const onMouseMove = (e: MouseEvent) => {
-            if (!isClicked.current)
+            if (!isClicked.current || !coords)
                 return;
 
-            const nextX = e.clientX - coords.current.startX + coords.current.lastX;
-            const nextY = e.clientY - coords.current.startY + coords.current.lastY;
+            const nextX = e.clientX - coords.startX + coords.lastX;
+            const nextY = e.clientY - coords.startY + coords.lastY;
             box.style.left = `${nextX}px`;
             box.style.top = `${nextY}px`;
         }
@@ -128,7 +136,7 @@ const StampControl: React.FC<IProps> = ({ api }) => {
 
     //#region Elements
     function showStamps() {
-        entity.StampInfostarkov
+        stampInfo
             .filter(row => row.PageNumber == currentPageNumber)
             .map(row => {
                 let mainDiv = document.getElementById(`dynamic-html${row.Id}`);
@@ -136,8 +144,11 @@ const StampControl: React.FC<IProps> = ({ api }) => {
                     let htmlObject = document.createElement('div');
                     htmlObject.innerHTML = row.StampHtml;
                     htmlObject.className = 'stamp';
-                    htmlObject.onmouseenter = function () { setCurrentStampId(row.Id) };
+                    htmlObject.onmousedown = function() {
+                        setCurrentStampId(row.Id);
+                    };
                     mainDiv?.appendChild(htmlObject);
+                    updateStampCoords(row);
                 }
                 else {
                     mainDiv?.childNodes.forEach((node) => {
@@ -168,22 +179,20 @@ const StampControl: React.FC<IProps> = ({ api }) => {
         });
     }
 
-    function updateCoordsFromEntity() {
-        entity.StampInfostarkov
-            .filter(row => row.PageNumber == currentPageNumber)
-            .map((row) => {
-                var boxRef = boxRefs.find(x => x.stampId == row.Id)?.boxRef;
-                if (!boxRef?.current)
-                    return;
+    function updateStampCoords(row: IStampInfoRow) {
+        var currentStamp = boxRefs.find(x => x.stampId == row.Id);
+        var coords = currentStamp?.coords.current;
+        var boxRef = currentStamp?.boxRef.current;
+        if (!boxRef || !coords)
+            return;
 
-                let x = dotToPx(row?.CoordX ?? 0);
-                let y = dotToPx(row?.CoordY ?? 0);
-                setCoordsText(`X: ${row?.CoordX.toFixed(1) ?? 0}, Y: ${row?.CoordY.toFixed(1) ?? 0}`);
-                coords.current.lastX = x;
-                coords.current.lastY = y;
-                boxRef.current.style.left = `${x}px`;
-                boxRef.current.style.top = `${y}px`;
-            });
+        let x = dotToPx(row?.CoordX ?? 0);
+        let y = dotToPx(row?.CoordY ?? 0);
+        setCoordsText(`X: ${row?.CoordX.toFixed(1) ?? 0}, Y: ${row?.CoordY.toFixed(1) ?? 0}`);
+        coords.lastX = x;
+        coords.lastY = y;
+        boxRef.style.left = `${x}px`;
+        boxRef.style.top = `${y}px`;
     }
 
     function setNextPageNumber(isNext: boolean) {
@@ -232,7 +241,7 @@ const StampControl: React.FC<IProps> = ({ api }) => {
             <br />
             <div id='page' className='page' ref={containerRef}>
                 {
-                    entity.StampInfostarkov
+                    stampInfo
                         .filter(row => row.PageNumber == currentPageNumber)
                         .map((row) => {
                             return (
